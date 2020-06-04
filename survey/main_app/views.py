79b3ec500bb20af76da71d888ae3c3418aa4a4aa
Egor Lukyanov <egor.lukyanov@raziogroup.com>
2020-06-04 14:29:12 +0300

    feat: Additional check

--- survey/main_app/views.py
+++ survey/main_app/views.py
@@ -1,11 +1,7 @@
-from django.utils import timezone
-from django.shortcuts import render
-
 from rest_framework import viewsets
 from rest_framework import permissions
 from rest_framework.response import Response
 
-from .models import *
 from .serializers import *
 
 
