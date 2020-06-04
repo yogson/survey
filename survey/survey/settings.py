8b50f0b6dd429254fe750d8f5d763b27db646d88
Egor Lukyanov <egor.lukyanov@raziogroup.com>
2020-06-04 10:49:57 +0300

    feat: Authentication

--- survey/survey/settings.py
+++ survey/survey/settings.py
@@ -105,6 +105,13 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ]
+}
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
